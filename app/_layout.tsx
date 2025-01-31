719b40504e027b8b7b3be515a94183da87960c7e
Bernard Iorver <53957336+benion28@users.noreply.github.com>
2025-01-31 16:40:44 +0100

    Starts Lesson 4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import React, { useEffect } from 'react';
+import { Appearance } from 'react-native';
+import { Colors } from '@/constants/Colors';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 const { Screen } = Stack
+const { Fragment } = React
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = Appearance.getColorScheme()
+  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -30,15 +31,20 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Screen name='index' options={{ title: 'Home', headerShown: false }} />
-        <Screen name='contact' options={{ title: 'Contact Us' }} /> */}
-        <Screen name="(coffee)" options={{ headerShown: false }} />
-        <Screen name="+not-found" />
+    <Fragment>
+      <Stack 
+        screenOptions={{ headerStyle: { 
+          backgroundColor: theme.headerBackground }, 
+          headerTintColor: theme.text,
+          headerShadowVisible: false
+        }}
+      >
+        <Screen name="index" options={{ headerShown: false, title: 'Home' }} />
+        <Screen name="contact" options={{ headerShown: true, title: 'Contact', headerTitle: 'Contact Us' }} />
+        <Screen name="menu" options={{ headerShown: true, title: 'Menu', headerTitle: 'Coffee Shop Menu' }} />
+        <Screen name="+not-found"  options={{ headerShown: false }}/>
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    </Fragment>
+  )
 }
